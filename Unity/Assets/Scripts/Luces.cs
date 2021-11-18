0e0e047ebaa01fb12fd548f1ddd2a60eb1817976
Adriana Lpz <59716935+adrianalpzs@users.noreply.github.com>
2021-11-18 16:39:23 -0600

    unity

---
+++ Unity/Assets/Scripts/Luces.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Luces : MonoBehaviour
+{
+
+    public GameObject red;
+    public GameObject green;
+    public GameObject yellow; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(ActivationRoutine());
+
+    }
+
+    private IEnumerator ActivationRoutine(){
+
+
+        green.SetActive(false);
+        yellow.SetActive(false);
+        red.SetActive(true);
+
+
+        yield return new WaitForSeconds(5);
+        red.SetActive(false);
+        green.SetActive(true);
+
+
+        yield return new WaitForSeconds(5);
+        green.SetActive(false);
+        yellow.SetActive(true);
+
+
+        yield return new WaitForSeconds(3);
+        yellow.SetActive(false);
+        red.SetActive(true);
+
+
+    }
+}
